60a9b6c10248e2bc1e7b9f29a4dba0b1e4a2daa9
projases <140944318+projases@users.noreply.github.com>
2024-02-23 20:34:27 +0100

    comments and structure

--- components/advSearch.js
+++ components/advSearch.js
@@ -1,50 +1,53 @@
-// document.querySelector('.advSearch').addEventListener('mouseover',() =>{
-import './style.css'
+import "./style.css";
+//Genera un menu desplegable
 function advSearch(iphones) {
-  const menuAdvSearch = document.createElement('div')
-  menuAdvSearch.classList.add('menuAdvSearch')
-  menuAdvSearch.appendChild(sellerFilter(iphones))
-  menuAdvSearch.appendChild(priceFilter())
-  return menuAdvSearch
+  const menuAdvSearch = document.createElement("div");
+  menuAdvSearch.classList.add("menuAdvSearch");
+  menuAdvSearch.appendChild(sellerFilter(iphones));
+  menuAdvSearch.appendChild(priceFilter());
+  return menuAdvSearch;
 }
 
+// genera un menu select por vendedor
 export function sellerFilter(iphones) {
-  const sellers = new Set(iphones.map((iphone) => iphone.seller))
-  const selectMenu = document.createElement('select')
-  selectMenu.classList.add('selectMenu')
-  const shop = document.createElement('option')
-  shop.textContent = '----'
-  selectMenu.appendChild(shop)
+  const sellers = new Set(iphones.map((iphone) => iphone.seller));
+  const selectMenu = document.createElement("select");
+  selectMenu.classList.add("selectMenu");
+  const shop = document.createElement("option");
+  shop.textContent = "-seller-";
+  selectMenu.appendChild(shop);
   for (const seller of sellers) {
-    const shop = document.createElement('option')
-    shop.textContent = seller
-    selectMenu.appendChild(shop)
+    const shop = document.createElement("option");
+    shop.textContent = seller;
+    selectMenu.appendChild(shop);
   }
-  return selectMenu
+  return selectMenu;
 }
+// genera un input y un boton para buscador por precio
 export function priceFilter() {
   // const prices = iphones.map(iphone => iphone.price)
-  const priceFilterContainer = document.createElement('div')
-  priceFilterContainer.classList.add('priceSearch')
-  const search = document.createElement('input')
-  search.setAttribute('type', 'text')
-  search.setAttribute('placeholder', 'Buscar')
-  search.classList.add('searchPrice')
-  const go = document.createElement('button')
-  go.textContent = 'Go'
-  go.classList.add('goButton')
-  priceFilterContainer.appendChild(search)
-  priceFilterContainer.appendChild(go)
-  return priceFilterContainer
+  const priceFilterContainer = document.createElement("div");
+  priceFilterContainer.classList.add("priceSearch");
+  const search = document.createElement("input");
+  search.setAttribute("type", "text");
+  search.setAttribute("placeholder", "Buscar por precio");
+  search.classList.add("searchPrice");
+  const go = document.createElement("button");
+  go.textContent = "Go";
+  go.classList.add("goButton");
+  priceFilterContainer.appendChild(search);
+  priceFilterContainer.appendChild(go);
+  return priceFilterContainer;
 }
-
+//Genera un botón para borrar búsqueda
 export function clearResults() {
   // const clearResultsContainer = document.createElement('div')
-  const clearResultsButton = document.createElement('button')
-  clearResultsButton.textContent = 'Clear'
+  const clearResultsButton = document.createElement("button");
+  clearResultsButton.textContent = "Clear";
   // clearResultsContainer.classList.add('clearDiv')
-  clearResultsButton.classList.add('clearButton')
+  clearResultsButton.classList.add("clearButton");
   // clearResultsContainer.appendChild(clearResultsButton)
-  return clearResultsButton
+  return clearResultsButton;
 }
-export default advSearch
+
+export default advSearch;
--- main.js
+++ main.js
@@ -1,33 +1,33 @@
-import './style.css'
+import "./style.css";
 // const MAX_OPEN_CARDS = 2
-import products from './iphones'
+import products from "./iphones";
 import advSearch, {
   sellerFilter,
   priceFilter,
-  clearResults
-} from './components/advSearch.js'
+  clearResults,
+} from "./components/advSearch.js";
 // Crear una variable contenedora de iphones
-const iphoneContainer = document.querySelector('#iPhones')
+const iphoneContainer = document.querySelector("#iPhones");
 //crear menu desplegable
-const searchComponent = advSearch(products)
+const searchComponent = advSearch(products);
 // append el menú al header
-document.querySelector('.header').append(searchComponent)
+document.querySelector(".header").append(searchComponent);
 //que no se vea el menú
-searchComponent.style.display = 'none'
-const clearPopUp = clearResults()
-document.body.appendChild(clearPopUp)
-clearPopUp.style.display = 'none'
+searchComponent.style.display = "none";
+const clearPopUp = clearResults();
+document.body.appendChild(clearPopUp);
+clearPopUp.style.display = "none";
 
-// Create card function
+// Crear tarjeta de iphone
 function createPhoneCard(iphone) {
   //crear tarjeta
-  const card = document.createElement('div')
-  card.classList.add('card')
+  const card = document.createElement("div");
+  card.classList.add("card");
 
   //Crear un div con la información del iphone
-  const back = document.createElement('div')
-  back.classList.add('back')
-  const iphoneInfo = document.createElement('p')
+  const back = document.createElement("div");
+  back.classList.add("back");
+  const iphoneInfo = document.createElement("p");
 
   iphoneInfo.textContent = `
     Name: ${iphone.name}
@@ -35,103 +35,110 @@ function createPhoneCard(iphone) {
     Stars: ${iphone.stars}
     Reviews: ${iphone.reviews}
     Seller: ${iphone.seller}
-`
-  // iphoneInfo.appendChild(textNode)
-  back.appendChild(iphoneInfo)
+`;
+  // Información de iphone en cara reversa
+  back.appendChild(iphoneInfo);
   // Crear cara frontal con la foto
-  const front = document.createElement('div')
-  front.classList.add('front')
-  front.style.backgroundImage = `url(${iphone.image})`
+  const front = document.createElement("div");
+  front.classList.add("front");
+  front.style.backgroundImage = `url(${iphone.image})`;
 
   //poner las caras de las tarjeta como hijos de la tarjeta
-  card.appendChild(back)
-  card.appendChild(front)
+  card.appendChild(back);
+  card.appendChild(front);
 
-  return card
+  return card;
 }
 //flip the card
 function flipCard(card) {
-  card.addEventListener('click', () => {
-    card.classList.toggle('flipped')
-  })
+  card.addEventListener("click", () => {
+    card.classList.toggle("flipped");
+  });
 }
-
-//create cards and add click event
-products.forEach((iphone) => {
-  const card = createPhoneCard(iphone)
-  flipCard(card)
-  iphoneContainer.append(card)
-})
-
-function renderSearchResults(searchResults) {
-  // borrar busqueda anterior
-  iphoneContainer.innerHTML = ''
-  // renderizar resultados searchResults
-  searchResults.forEach((iphone) => {
-    const card = createPhoneCard(iphone)
-    flipCard(card)
-    iphoneContainer.append(card)
-  })
-}
-document.querySelector('.search').addEventListener('input', () => {
-  const query = document.querySelector('.search').value.toLowerCase()
-  const searchResults = products.filter((iphone) => {
-    let iphoneProperties = Object.values(iphone)
-    for (let i = 0; i < iphoneProperties.length - 1; i++) {
-      console.log(iphoneProperties[i])
-      if (iphoneProperties[i].toString().toLowerCase().includes(query)) {
-        return true
-      }
-    }
-    return false
-  })
-  renderSearchResults(searchResults)
-})
-
-document.querySelector('.advSearch').addEventListener('mouseover', () => {
-  searchComponent.style.display = 'flex'
-  iphoneContainer.classList.add('blurred')
-})
-searchComponent.addEventListener('mouseleave', () => {
-  searchComponent.style.display = 'none'
-  iphoneContainer.classList.remove('blurred')
-})
-
+// buscador por precio
 function priceSearch(products, refPrice) {
   const searchResults = products.filter((iphone) => {
-    console.log(iphone.price)
-    return parseFloat(iphone.price) <= refPrice
-  })
-  return searchResults
+    console.log(iphone.price);
+    return parseFloat(iphone.price) <= refPrice;
+  });
+  return searchResults;
 }
+//buscador por vendedor
 function sellerSearch(products, refSeller) {
   const searchResults = products.filter((iphone) => {
-    return iphone.seller === refSeller
-  })
-  return searchResults
+    return iphone.seller === refSeller;
+  });
+  return searchResults;
 }
-document.querySelector('.goButton').addEventListener('click', () => {
-  let refPrice = parseFloat(document.querySelector('.searchPrice').value)
-  const searchResults = priceSearch(products, refPrice)
-
-  renderSearchResults(searchResults)
-  clearPopUp.style.display = 'block'
-})
-document.querySelector('.selectMenu').addEventListener('change', () => {
-  let refSeller = document.querySelector('.selectMenu').value
-  const searchResults = sellerSearch(products, refSeller)
-  renderSearchResults(searchResults)
-  clearPopUp.style.display = 'block'
-})
-
+// borrar busqueda y parámetros de busqueda avanzada
 function clearAll() {
-  document.querySelector('.searchPrice').value = ''
-  document.querySelector('.selectMenu').selectedIndex = 0
-  renderSearchResults(products)
+  document.querySelector(".searchPrice").value = "";
+  document.querySelector(".selectMenu").selectedIndex = 0;
+  renderSearchResults(products);
 }
 
-// document.querySelector('.clearButton')
-clearPopUp.addEventListener('click', () => {
-  clearAll()
-  clearPopUp.style.display = 'none'
-})
+// generar cartas iphone y asignarles el flip
+products.forEach((iphone) => {
+  const card = createPhoneCard(iphone);
+  flipCard(card);
+  iphoneContainer.append(card);
+});
+
+// renderizadora de iphones
+function renderSearchResults(searchResults) {
+  // borrar busqueda anterior
+  iphoneContainer.innerHTML = "";
+  // renderizar resultados searchResults
+  searchResults.forEach((iphone) => {
+    const card = createPhoneCard(iphone);
+    flipCard(card);
+    iphoneContainer.append(card);
+  });
+}
+
+// ------------------Event listeners-------------------------------
+document.querySelector(".search").addEventListener("input", () => {
+  const query = document.querySelector(".search").value.toLowerCase();
+  const searchResults = products.filter((iphone) => {
+    let iphoneProperties = Object.values(iphone);
+    for (let i = 0; i < iphoneProperties.length - 1; i++) {
+      console.log(iphoneProperties[i]);
+      if (iphoneProperties[i].toString().toLowerCase().includes(query)) {
+        return true;
+      }
+    }
+    return false;
+  });
+  renderSearchResults(searchResults);
+});
+
+// Toggle menu de busqueda avanzada
+document.querySelector(".advSearch").addEventListener("mouseover", () => {
+  searchComponent.style.display = "flex";
+  iphoneContainer.classList.add("blurred");
+});
+searchComponent.addEventListener("mouseleave", () => {
+  searchComponent.style.display = "none";
+  iphoneContainer.classList.remove("blurred");
+});
+
+// botón de búsqueda por precio
+document.querySelector(".goButton").addEventListener("click", () => {
+  let refPrice = parseFloat(document.querySelector(".searchPrice").value);
+  const searchResults = priceSearch(products, refPrice);
+  renderSearchResults(searchResults);
+  clearPopUp.style.display = "block";
+});
+// selección de menú select
+document.querySelector(".selectMenu").addEventListener("change", () => {
+  let refSeller = document.querySelector(".selectMenu").value;
+  const searchResults = sellerSearch(products, refSeller);
+  renderSearchResults(searchResults);
+  clearPopUp.style.display = "block";
+});
+
+// botón de borrar busqueda actual
+clearPopUp.addEventListener("click", () => {
+  clearAll();
+  clearPopUp.style.display = "none";
+});
